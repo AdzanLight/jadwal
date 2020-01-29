82eb3dbebee03f05c9f6fe5e4fda8d34669d60d0
Adzanlight <53359666+AdzanLight@users.noreply.github.com>
2020-01-29 19:47:54 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,50 @@
+#Silahkan Pelajari Ntod
+#kontol
+#memek
+#heheeh
+
+
+
+
+import os
+from time import sleep
+import socket,struct,time,os,sys
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1/ 2)
+        
+#banner
+os.system('clear')
+print('''  \033[93m  ________________________________''')
+print('''  \033[92m        author : AdzanLight            \033[92m  ''')
+
+print('''  \033[92m        Team: NewbieTesterID       \033[92m  ''')
+print('''  \033[92m           Mata Pelajaran            \033[92m  ''')
+
+print('''  \033[93m  ________________________________   ''')
+
+
+
+
+
+
+
+slowprint(' \033[94m  Ini Hanyalah Tols Untuk Para Pelajar')
+sleep(1)
+slowprint('  \033[94m Memudahkan Mengatahui Mata Pelajaran Sekolah')
+sleep(1)
+slowprint(' \033[94m  Jika Kalian Mau mempelajarin pembuatan Scrip Maka Sournya Saya tidak Encrip')
+sleep(1)
+slowprint ('  \033[94m Enjooy')
+sleep(1)
+slowprint ('  \033[94m  Ini hanyalah intro jika nak menjalankan tols nya ketik saja ')
+sleep (1)
+slowprint ('  \033[94m   bash jadwal.sh ')
+sleep (2)
+import os, sys
+os.system('bash jadwal.sh')
+
+
